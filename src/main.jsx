8df0bbd2a511b5e56a957cad9fd936067589d85f
maxpleaner <maxpleaner@gmail.com>
2025-12-07 15:43:06 -0800

    push

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,11 @@ import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
 
+const basename = import.meta.env.VITE_BASE_URL || '/'
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename="/New_Personal_Website_2025/">
+    <BrowserRouter basename={basename}>
     <App />
     </BrowserRouter>
   </StrictMode>,
