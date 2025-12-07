0ccd581ac774434fdd30fdbca0a2bfb4934799cb
maxpleaner <maxpleaner@gmail.com>
2025-12-07 15:39:58 -0800

    build

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/New_Personal_Website_2025/">
     <App />
     </BrowserRouter>
   </StrictMode>,
