da9454381a2ef1dd1fcd410f139edbb334e3b289
maxpleaner <maxpleaner@gmail.com>
2025-12-09 09:28:34 -0800

    tmp

--- src/Pages/Projects/ProjectCard.jsx
+++ src/Pages/Projects/ProjectCard.jsx
@@ -12,7 +12,7 @@ export function ProjectCard({ project }) {
   return (
     <a href={project.url} target="_blank" rel="noopener noreferrer" style={{ textDecoration: 'none', color: 'inherit' }}>
       <div style={{ width: CARD_WIDTH, height: CARD_HEIGHT, display: 'flex', flexDirection: 'column', position: 'relative', backgroundColor: '#000', padding: '15px', border: '2px solid rgba(255, 255, 255, 0.5)' }}>
-        <h3 style={{ marginTop: 0, marginBottom: TITLE_MARGIN_BOTTOM, textAlign: 'center' }}>{project.name}</h3>
+        <h3 style={{ fontSize: '1.0em', marginTop: 0, marginBottom: TITLE_MARGIN_BOTTOM, textAlign: 'center' }}>{project.name}</h3>
         {project.thumbnailType === 'image' && (
           <div style={{ flex: `0 0 ${IMAGE_HEIGHT}`, overflow: 'hidden', marginBottom: IMAGE_MARGIN_BOTTOM, border: `1px solid ${BORDER_COLOR_IMAGE}` }}>
             <img src={project.thumbnail} alt={project.name} style={{ width: '100%', height: '100%', objectFit: 'cover', display: 'block' }} />
